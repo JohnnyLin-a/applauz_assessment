cc9a157d242c6175950cc979d9578e438dd7c13c
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-05 12:43:43 -0500

    Add folder structure to project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Joi = require('joi');
+
+
+const startServer = () => {
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+    const port = process.env.PORT || 3000
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}...`);
+    })
+}
+
+
+startServer();
