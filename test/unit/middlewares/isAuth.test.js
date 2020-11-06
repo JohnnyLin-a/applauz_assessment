929a9971f43155f1791b9a68230ea4a6468013cc
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-06 12:22:34 -0500

    Add tests for isAuth middleware

---
+++ test/unit/middlewares/isAuth.test.js
@@ -0,0 +1,57 @@
+const isAuth = require('../../../src/api/middlewares/isAuth');
+const { NextFunction, Request, Response } = require('express');
+const config = require('../../../src/config');
+
+describe("isAuth middleware", () => {
+    let mockRequest = {};
+    let mockResponse = {};
+    let nextFunction = jest.fn();
+
+    beforeEach(() => {
+        mockRequest = { body: {} };
+        mockResponse = {
+            json: jest.fn()
+        };
+    });
+
+    test('undefined api_key', async () => {
+        const expectedResponse = { error: "You are unauthorized to proceed (No api_key)" };
+        isAuth(mockRequest, mockResponse, nextFunction)
+
+        expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
+
+    test('null api_key', async () => {
+        mockRequest.body.api_key = null;
+        const expectedResponse = { error: "You are unauthorized to proceed (No api_key)" };
+        isAuth(mockRequest, mockResponse, nextFunction)
+
+        expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
+
+    test('empty string api_key', async () => {
+        mockRequest.body.api_key = "";
+        const expectedResponse = { error: "You are unauthorized to proceed (Wrong api_key)" };
+        isAuth(mockRequest, mockResponse, nextFunction)
+
+        expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
+
+    test('wrong api_key', async () => {
+        // Concatenating the corerect key to ensure it is wrong.
+        mockRequest.body.api_key = "WRONG_API_KEY" + config.api_key;
+        const expectedResponse = { error: "You are unauthorized to proceed (Wrong api_key)" };
+        isAuth(mockRequest, mockResponse, nextFunction)
+
+        expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
+
+    test('correct api_key', async () => {
+        mockRequest.body.api_key = config.api_key;
+        isAuth(mockRequest, mockResponse, nextFunction)
+
+        expect(nextFunction).toHaveBeenCalled();
+    });
+})
+
+
