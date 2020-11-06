27e395aab4da7939b71bbc6d9351324c1f205664
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-05 23:02:53 -0500

    Remove unnecessary console logs

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -6,11 +6,6 @@ module.exports = (app) => {
   app.use('/users', route);
 
   route.post('/me', middlewares.isAuth, (req, res) => {
-    console.log(req.params);
-    console.log(req.query);
-    console.log(req.body);
-    //   params: {},
-    // query: {},
     return res.json({ user: req.body.currentUser }).status(200);
   });
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const startServer = async () => {
 
     await loaders({ expressApp: app });
 
-    console.log("api key ", process.env.API_KEY);
-
     const port = process.env.PORT || 3000
     app.listen(port, () => {
         console.log(`Listening on port ${port}...`);
