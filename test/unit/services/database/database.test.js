155e2ab320a1b0af264bf89f37f40ac9070f70f9
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-06 20:23:01 -0500

    Change database test structure

--- test/unit/services/database/database.test.js
+++ test/unit/services/database/database.test.js
@@ -1,17 +1,25 @@
-const { pool } = require('../../../../src/services/database');
+const { Pool } = require('pg');
+const { postgres_config } = require('../../../../src/config');
 
-describe("Service: database.pool", () => {
-    afterAll(async () => {
-        return await pool.end();
-    })
-    test('connection', async () => {
-        let success = true;
-        pool.query("SELECT NOW() AS now", (err, res) => {
-            if (err) {
+describe("Service: database", () => {
+    describe('pool', () => {
+        let pool = null;
+        beforeAll(() => {
+            pool = new Pool(postgres_config);
+        })
+        afterAll(async () => {
+            await pool.end();
+        })
+        test('connection', async () => {
+            let success = null;
+            try {
+                await pool.query("SELECT NOW() AS now");
+                success = true;
+            } catch (err) {
+                console.log("Connection error", err);
                 success = false;
             }
-        })
-
-        expect(success).toBe(true);
-    });
+            expect(success).toBe(true);
+        });
+    })
 })
