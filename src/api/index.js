071e5b63c8bba9f9db20292dbcf1facdc12d03aa
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-06 20:34:53 -0500

    Remove testAuth route, unused

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
-const testAuth = require('./routes/testAuth');
 const user = require('./routes/user');
 
 module.exports = () => {
 	const app = Router();
 	user(app);
-	testAuth(app);
 
 	return app
 }
