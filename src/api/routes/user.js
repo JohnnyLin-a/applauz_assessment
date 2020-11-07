efeb9900875ef1d70655fefeb9768b0e4e29fd8a
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-06 21:34:30 -0500

    Add insert user, and WIP for other routes

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,11 +1,36 @@
 const { Router, Request, Response } = require('express');
-const middlewares = require('../middlewares')
+const middlewares = require('../middlewares');
+const { createUser } = require('../../services/auth');
 const route = Router();
 
 module.exports = (app) => {
   app.use('/users', route);
 
-  route.post('/me', middlewares.isAuth, (req, res) => {
-    return res.json({ user: req.body.currentUser }).status(200);
+  route.get('/', middlewares.isAuth, (req, res) => {
+    return res.json({ req: "GET" }).status(200);
+  });
+
+  route.get('/:id', middlewares.isAuth, (req, res) => {
+    return res.json({ req: "GET" }).status(200);
+  });
+
+  route.post('/', middlewares.isAuth, async (req, res) => {
+    const result = await createUser(req.body);
+    if (result instanceof Error) {
+      if (result.isJoi) {
+        res.statusCode = 400;
+        res.json({ error: result.details[0].message });
+        return;
+      }
+      // if it isn't Joi, it's Postgres
+      res.statusCode = 500;
+      res.send({ error: result.detail });
+      return;
+    }
+    return res.json({ success: true, id: result });
+  });
+
+  route.delete('/', middlewares.isAuth, (req, res) => {
+    return res.json({ req: "DELETE" }).status(200);
   });
 };
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,12 +1,30 @@
 const config = require('../config');
+const Joi = require('joi');
+const { insertUser, deleteUser, getUser } = require('../services/database/user');
 
-const authService = {
-    validateApiKey: api_key => {
-        if (config.api_key === api_key) {
-            return true;
-        }
-        return false;
-    },
+const validateApiKey = api_key => {
+    if (config.api_key === api_key) {
+        return true;
+    }
+    return false;
 }
 
-module.exports = authService
+const createUser = async (user) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+
+    const { error } = schema.validate(user);
+    if (error) {
+        return error;
+    }
+
+    const queryResult = await insertUser(user.name);
+    if (queryResult instanceof Error) {
+        return queryResult;
+    }
+    return queryResult.id;
+
+}
+
+module.exports = { validateApiKey, createUser }
