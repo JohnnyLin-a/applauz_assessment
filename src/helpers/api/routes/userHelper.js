b4b23f5b1d3532fb9d7ca29e824e560f47a30a28
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-07 18:03:30 -0500

    Fix variable name typo

--- src/helpers/api/routes/userHelper.js
+++ src/helpers/api/routes/userHelper.js
@@ -3,7 +3,7 @@
 const validateError = (result) => {
     if (result instanceof Error) {
         if (result.isJoi) {
-            return { statusCode: 400, error: { error: result.details[0].message } };
+            return { statusCode: 400, errorToSend: { error: result.details[0].message } };
         }
         // if it isn't Joi, it's Postgres
         return { statusCode: 500, errorToSend: { error: result.detail } };
