5d0eea7bf65678758b0ae7a4ff1cf73c39308522
Johnny Lin <johnny.lin.prime@gmail.com>
2020-11-08 18:38:05 -0500

    Add web-client to express

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const path = require('path');
 const routes = require('../api');
 const config = require('../config');
 
 const expressLoader = async ({ app }) => {
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
+    // app.get('/', (req, res) => {
+    //     res.send('Hello World!');
+    // });
+
+    app.use(express.static(path.join(__dirname, '../../web-client/build')));
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../../web-client/build', 'index.html'));
     });
-    app.head('/', (req, res) => {
-        res.status(200).end();
-    });
+    // app.head('/', (req, res) => {
+    //     res.status(200).end();
+    // });
     app.use(express.json());
     app.use("/api", routes());
 
